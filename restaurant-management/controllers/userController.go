3f17e8597aabb0836e1aa4e47e5de0ba7a7eeee2
seika-afk <artistnoob6@gmail.com>
2026-04-17 21:29:59 +0530

    added::controllers

---
+++ restaurant-management/controllers/userController.go
@@ -0,0 +1,84 @@
+package controllers
+
+import (
+
+	"github.com/gin-gonic/gin"
+)
+
+
+
+
+
+func GetUsers() gin.HandlerFunc{
+
+
+	return func(c *gin.Context){
+
+
+
+
+	}
+}
+
+
+func GetUser() gin.HandlerFunc{
+
+return func( c *gin.Context){
+
+	}
+
+
+}
+
+
+func Signup() gin.HandlerFunc{
+
+	return func(c *gin.HandlerFunc){
+
+
+	}
+}
+
+
+func Login() gin.HandlerFunc{
+
+	return func(c *gin.Context){
+
+
+	}
+
+}
+
+func HashPassword(psd string ) string{
+
+
+
+
+
+}
+
+
+//userPassword and providedPassword
+func VerifyPassword(uP string, pP string)(bool,string){
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
