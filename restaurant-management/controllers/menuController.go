f0e20aed5776a2818c9fee75aaad90e0e3579058
seika-afk <artistnoob6@gmail.com>
2026-04-21 18:44:58 +0530

    added:menu_controller: GetMenus

--- restaurant-management/controllers/menuController.go
+++ restaurant-management/controllers/menuController.go
@@ -4,18 +4,38 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"rtm/database"
+	"rtm/models"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetMenus() gin.HandlerFunc{
 
 
 return func(c *gin.Context){
+// get context and cancel
+		ctx,cancel := context.WithTimeout(context.Background(),100*time.Second)
+		// get menu id 
+		menu_id := c.Param("menu_id")
+		//create a menu model 
+		var menu models.Menu
+		// try finding the foodCollection with that menu id and put in menu model 
+		err:= foodCollection.FindOne(ctx,bson.M{"menu_id":menu_id}).Decode(&menu)
+
+	
+		// put cancel in stack 
+	defer cancel()
+		// err 
+		if err != nil{
+
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured while fetching menu"})
+		}
+		//return 
+c.JSON(http.StatusOK,menu)
+		
+		
 	}
 	}
 
@@ -47,10 +67,8 @@ func GetMenu() gin.HandlerFunc{
 
 
 func CreateMenu() gin.HandlerFunc{
-
-
-
 	return func(c *gin.Context){
+
 
 	}
 }
