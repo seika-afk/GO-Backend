f933e736e4ad496c8ab9b431a5c50750a6f57b61
seika-afk <artistnoob6@gmail.com>
2026-04-25 22:04:04 +0530

    completed GetFoods Handler function

--- restaurant-management/controllers/foodController.go
+++ restaurant-management/controllers/foodController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"rtm/database"
 	"rtm/models"
@@ -18,168 +19,161 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-var foodCollection *mongo.Collection= database.OpenCollection(database.Client,"food")
-var menuCollection *mongo.Collection = database.OpenCollection(database.Client,"menu")
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 var validate = validator.New()
 
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-func GetFoods() gin.HandlerFunc{
-return func(c *gin.Context){
+		// get context
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
-// get context 
-		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
-
-	//pagination -> avoiding sending too much data to frontend
-		recordPerPage,err:= strconv.Atoi(c.Query("recordPerPage"))
-		if err != nil || recordPerPage<1{
-			recordPerPage= 10
+		//pagination -> avoiding sending too much data to frontend
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10
 		}
-		page,err:= strconv.Atoi(c.Query("page"))
-		if err != nil || page<1{
-			page=1
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
 		}
-		startIndex := (page-1)*recordPerPage
-		startIndex,err = strconv.Atoi(c.Query("startIndex"))
-			//match stage 
-				matchStage := bson.D{{"$match",bson.D{{}}}}
-		// group stage 
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+		//match stage
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+		// group stage
 		groupStage := bson.D{{
 			"$group", bson.D{{
 				"_id", bson.D{{
-					"_id", "null"}}}, 
-					{"total_count", bson.D{{"$sum", 1}}}, 
-			{"data", bson.D{{"$push", "$$ROOT"}}}}}}
-		
+					"_id", "null"}}},
+				{"total_count", bson.D{{"$sum", 1}}},
+				{"data", bson.D{{"$push", "$$ROOT"}}}}}}
 
 		// projectStage
 
+		projectStage := bson.D{
+			{"$project", bson.D{
 
-						projectStage := bson.D{
-										{"$project",bson.D{
+				{"_id", 0},
+				{"total_count", 1},
+				{"food_items", bson.D{
+					{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+			}}}
 
-										{"_id",0},
-										{"total_count",1},
-										{"food_items",bson.D{
-													{"$slice",[]interface{}{"$data",startIndex,recordPerPage}}}},
-	}}}
+		res, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage,
+		})
+		defer cancel()
 
-
-				// TODO : Understand this aggregation step 
-
-
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.h{"error": "Error occured"})
+		}
+		var allFoods []bson.M
+		if err := res.All(ctx, &allFoods); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allFoods[0])
 	}
 
 }
 
+func GetFood() gin.HandlerFunc {
 
-
-func GetFood() gin.HandlerFunc{
-
-	return  func(c *gin.Context){
+	return func(c *gin.Context) {
 		// get ctx with timeout 100
-		ctx,cancel := context.WithTimeout(context.Background(),100*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		// get food id from parameters
 		food_id := c.Param("food_id")
 		// define the struct food model
 		var food models.Food
-		// find the ID one and decode from json to struct 
-		err := foodCollection.FindOne(ctx,bson.M{"food_id":food_id}).Decode(&food)
-		//put in stack for canceling db connection 
+		// find the ID one and decode from json to struct
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": food_id}).Decode(&food)
+		//put in stack for canceling db connection
 		defer cancel()
 		// check err
-		if err != nil{
-			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured while fetching food item"})
-		
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching food item"})
+
 		}
 		// send back response
-		c.JSON(http.StatusOK,food)	
+		c.JSON(http.StatusOK, food)
 
 	}
 }
 
+func CreateFood() gin.HandlerFunc {
 
-func CreateFood() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		// get context and cancel
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		// define menu model adnd food model
 
-	return func(c *gin.Context){
-				// get context and cancel 
-					ctx, cancel := context.WithTimeout(context.Background(),100*time.Second)	
-				// define menu model adnd food model 
-
-				var menu models.Menu
-				var food models.Food
-			// if not able to convert json -> struct -> error
-				if err := c.BindJSON(&food); err!= nil{
-			c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
-				return 
-		}
-	
-	
-	// validate the struct 
-
-	validationErr := validate.Struct(food)
-	
-		// validation error 
-			if validationErr!= nil{
-			c.JSON(http.StatusBadRequest,gin.H{"error":validationErr.Error()})
+		var menu models.Menu
+		var food models.Food
+		// if not able to convert json -> struct -> error
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 
-		// find the menu with menu id   -> menu struct 
-		err:= menuCollection.FindOne(ctx,bson.M{"menu_id":food.Menu_id}).Decode(&menu)
+		// validate the struct
+
+		validationErr := validate.Struct(food)
+
+		// validation error
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+		}
+
+		// find the menu with menu id   -> menu struct
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
 		if err != nil {
 			msg := fmt.Sprintf("menu was not found")
 			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 
-		}		// create food's values -> created_at, updated_at , ID , food_id , price 
-		
-food.Created_at,_= time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
-food.Updated_at,_= time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
+		} // create food's values -> created_at, updated_at , ID , food_id , price
 
-food.ID= primitive.NewObjectID()
-	food.Food_id= food.ID.Hex()
+		food.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 
-var num = toFixed(*food.Price,2)
-		food.Price= &num
+		food.ID = primitive.NewObjectID()
+		food.Food_id = food.ID.Hex()
 
+		var num = toFixed(*food.Price, 2)
+		food.Price = &num
 
-		//try inserting 
+		//try inserting
 
-		res,errr:= foodCollection.InsertOne(ctx,food)
-
+		res, errr := foodCollection.InsertOne(ctx, food)
 
 		// accordingly give error or result
 
-if errr != nil{
-		msg := fmt.Sprintf("Food item was not created")
+		if errr != nil {
+			msg := fmt.Sprintf("Food item was not created")
 			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 
 		}
 
-
-
 		defer cancel()
-		c.JSON(http.StatusOK,res)
+		c.JSON(http.StatusOK, res)
 
 	}
 
-
 }
 
+func UpdateFood() gin.HandlerFunc {
 
-func UpdateFood() gin.HandlerFunc{
-
-	return func(c *gin.Context){
+	return func(c *gin.Context) {
 
 	}
 }
 
-
-
-
-func round(num float64)int{
+func round(num float64) int {
 
 }
 
-func toFixed(num float64,precision int) float64{
+func toFixed(num float64, precision int) float64 {
 }
